6343136e2a37d23069351a9a6b9116fab4c305bf
Sanjay Das <sanjaydas.32100@gmail.com>
2020-09-03 23:33:03 +0530

    Dev Profile separated

--- ApiGateway/src/main/java/com/microservice/platform/ApiGateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/microservice/platform/ApiGateway/ApiGatewayApplication.java
@@ -1,15 +1,42 @@
 package com.microservice.platform.ApiGateway;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
 
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-@EnableZuulProxy
+@Slf4j
+@RequiredArgsConstructor
 @SpringBootApplication
 public class ApiGatewayApplication {
 
+    private final Environment environment;
+
+    @PostConstruct
+    public void init() {
+        Set<String> profiles = Arrays.stream(environment.getActiveProfiles()).collect(Collectors.toSet());
+        log.info("Running Config Server with profiles {}", profiles);
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
+        SpringApplication app = new SpringApplication(ApiGatewayApplication.class);
+        SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+        addDefaultProfile(app, source);
+        app.run(args);
+    }
+
+    private static void addDefaultProfile(SpringApplication app, SimpleCommandLinePropertySource source) {
+        if (!source.containsProperty("spring.profiles.active") &&
+                !System.getenv().containsKey("SPRING_PROFILES_ACTIVE")) {
+
+            app.setAdditionalProfiles("dev");
+        }
     }
 }
--- ApiGateway/src/main/java/com/microservice/platform/ApiGateway/AppCloudConf.java
+++ ApiGateway/src/main/java/com/microservice/platform/ApiGateway/AppCloudConf.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableZuulProxy
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
