cb7a7e33cafedbe6fd794a32ae8338bf7c7db2c0
Sanjay Das <sanjaydas.32100@gmail.com>
2020-09-03 18:07:57 +0530

    Config Server Created

--- ConfigurationService/src/main/java/com/microservice/platform/ApiGateway/ConfigurationServiceApplication.java
+++ ConfigurationService/src/main/java/com/microservice/platform/ApiGateway/ConfigurationServiceApplication.java
@@ -1,17 +1,49 @@
 package com.microservice.platform.ApiGateway;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
 
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Slf4j
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableConfigServer
+@RequiredArgsConstructor
 public class ConfigurationServiceApplication {
 
+    private final Environment environment;
+
+    @PostConstruct
+    public void init() {
+        Set<String> profiles = Arrays.stream(environment.getActiveProfiles()).collect(Collectors.toSet());
+        log.info("Running Config Server with profiles {}", profiles);
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(ConfigurationServiceApplication.class, args);
+       // SpringApplication.run(ConfigurationServiceApplication.class, args);
+
+        SpringApplication app = new SpringApplication(ConfigurationServiceApplication.class);
+        SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+        addDefaultProfile(app, source);
+        app.run(args);
+    }
+
+    private static void addDefaultProfile(SpringApplication app, SimpleCommandLinePropertySource source) {
+        if (!source.containsProperty("spring.profiles.active") &&
+                !System.getenv().containsKey("SPRING_PROFILES_ACTIVE")) {
+
+            app.setAdditionalProfiles("dev");
+        }
     }
 
 }
